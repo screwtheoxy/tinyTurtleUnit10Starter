58a7998106976e458270318c8b520d9ef8fd01fc
Thomas Cross <thomas.corey.cross@gmail.com>
2017-02-01 23:24:52 +0000

    Hoping this works

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 TinyTurtle.apply(window);
 
-function square(){
-forward(50);
+function square(x){
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 right(90);
-forward(50);
+forward(x);
 }
 
+function triangle(x){
+    right(45);
+    forward(x);
+    right(110);
+    forward(x);
+    right(135);
+    forward(x);
+}
+
+function shape(size, angle){
+    forward(size);
+    right(angle);
+    forward(size);
+    right(angle);
+    forward(size);
+    right(angle);
+}
 // Type your function call below
 
 
-stamp();
+
+shape(25, 60);
+shape(25, 60);
+shape(30, 60);
+shape(30, 60);
+shape(35, 60);
+shape(35, 60);
+shape(40, 60);
+shape(40, 60);
